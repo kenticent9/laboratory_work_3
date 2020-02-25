56e87b59aae3b1280eb53e83cab94a55add3c095
kenticent9 <kenticent9@gmail.com>
2020-02-25 18:25:38 +0200

    Fixed bugs
    GET and POST methods somehow changed to lowercase — fixed; added exception when invalid Twitter username is entered

--- task_3/app.py
+++ task_3/app.py
@@ -5,20 +5,24 @@ from main import create_map, read_data
 app = Flask(__name__)
 
 
-@app.route("/", methods=['get', 'post'])
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    if request.method == 'get':
+    if request.method == 'GET':
         return render_template("index.html")
 
-    if request.method == 'post':
+    if request.method == 'POST':
         username = request.form['username']
-        create_map(read_data(username))
-        return redirect(url_for('friends_map'))
+        try:
+            create_map(read_data(username))
+            return redirect(url_for('friends_map'))
+        except:
+            return redirect(url_for('index'))
 
 
-@app.route('/map', methods=['get'])
+@app.route('/map', methods=['GET'])
 def friends_map():
     return render_template('map.html')
+
 
 if __name__ == '__main__':
     app.run()
